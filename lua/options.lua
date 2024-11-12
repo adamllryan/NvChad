930abacc17f2812c3ffd758eabdc20805d9fa1cc
Adam <adamllryan@gmail.com>
2024-11-12 12:17:01 -0500

    options and whichkey

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
+o.wrap = true
+o.listchars = "tab:| ,nbsp:+,trail:·,extends:→,precedes:←,eol:↲,space: "
